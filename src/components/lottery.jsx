d828c2087e399d585812a0b3fd84252b5aef3967
shantow-67 <shantow6767@gmail.com>
2023-09-05 11:27:27 +0600

    "updated"

--- src/components/lottery.jsx
+++ src/components/lottery.jsx
@@ -7,8 +7,13 @@ const Lottery = () => {
     const [result,setResult] = useState('')
 
     function handleaddName() {
-        setNameList([...nameList, name]);
+        if (name.length > 0) {
+            setNameList([...nameList, name]);
         setName('');
+        } else {
+            window.alert("Add a Name, Please")
+        }
+        
 
     }
     // const handleRemove = (index) => {
@@ -44,13 +49,13 @@ const Lottery = () => {
 
     return (
         <div>
-            <h1 className="text-5xl text-center py-4 bg-slate-300 text-blue-600 font-bold ">Lottery App</h1>
+            <h1 className="text-5xl text-center py-8 bg-gray-900 text-lime-400 font-bold ">Lottery App</h1>
             <div className=" flex flex-col md:flex md:flex-row justify-center py-8  gap-4">
                 <h3 className="text-3xl text-center font-semibold">write a name</h3>
                 <div className="flex justify-center">
-                    <input value={name} onChange={(e)=>setName(e.target.value)} onKeyPress={handleKeyPress} type="text" placeholder="Name...." className="border-2 border-blue-600 pl-2" />
+                    <input value={name} onChange={(e)=>setName(e.target.value)} onKeyDown={handleKeyPress} type="text" placeholder="Name...." className="border-2 border-blue-600 pl-2" />
 
-                <button onClick={handleaddName}  className="bg-blue-500 ml-2  text-white rounded-sm text-xl p-2">Add</button>
+                <button onClick={handleaddName}  className="bg-blue-500 ml-2  text-white rounded-md text-xl px-4 py-2">Add</button>
                 </div>
             </div>
 
@@ -71,12 +76,13 @@ const Lottery = () => {
                     </tbody>
                     
                 </table>
-                <div className="w-[80%] md:w-[40%] border-2 mx-auto md:mx-0 border-black flex justify-center items-center">
-                    <h1 className="text-5xl py-5 md:py-0 text-red-500 capitalize font-bold">{result}</h1>
+                <div className="w-[80%] md:w-[40%] border-2 mx-auto md:mx-0 border-black flex flex-col justify-center items-center">
+                    <p>Elected person is...</p>
+                    <h1 className="text-5xl py-5 md:py-0 text-gray-900 capitalize font-bold">{result}</h1>
                  </div>
             </div>
             <div className="flex justify-center">
-                <button onClick={lottery} className="bg-green-700 text-white text-center font-semibold text-2xl p-2 border-2 border-red-500  mt-4 rounded-xl">Select randomly</button>
+                <button onClick={lottery} className="bg-lime-400 text-gray-900 ring-2 ring-red-400 transform hover:scale-105 ease-in text-center font-semibold text-2xl p-2 border-2 border-red-500  mt-4 rounded-xl">Select randomly</button>
             </div>
 
 
